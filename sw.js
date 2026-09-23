c28a56cc7aba4665f0c53f276920bd89e0804eaf
therandomstatsguy <therandomstatsguy@gmail.com>
2026-09-24 08:19:25 +1000

    Add files via upload

--- sw.js
+++ sw.js
@@ -13,7 +13,7 @@
    launch behind; version.json (never cached, below) is what lets the page
    notice that and offer the reader an update. */
 
-var CACHE = 'rsg-202609230455';
+var CACHE = 'rsg-202609232213';
 var SHELL = [
   './',
   './index.html',
